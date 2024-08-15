4683ad767d31f87346e394a546ee53bab923a592
wangheng <1720466861@qq.com>
2024-08-15 23:11:48 +0800

    使用http进行后端调用

--- generated/core/OpenAPI.ts
+++ generated/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'https://localhost',
+    BASE: 'http://localhost',
     VERSION: '1.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
