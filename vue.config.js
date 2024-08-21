0a6375ebbfa72fd423ebb4a3e289bdc3d12af240
wangheng <1720466861@qq.com>
2024-08-21 14:25:18 +0800

    前端通过HTTPS提供服务

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    https: {
+      key: require("fs").readFileSync("./server.key"),
+      cert: require("fs").readFileSync("./server.crt"),
+    },
+  },
 });
